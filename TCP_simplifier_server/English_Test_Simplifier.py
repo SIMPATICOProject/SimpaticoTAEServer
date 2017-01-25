73e01776ae98901ad2dcab30267f2fdc66bd6ac3
ghpaetzold <ghpaetzold@outlook.com>
2017-01-25 13:25:41 +0000

    Fixed TCP LS server

--- TCP_simplifier_server/English_Test_Simplifier.py
+++ TCP_simplifier_server/English_Test_Simplifier.py
@@ -4,12 +4,12 @@ info = {}
 info['sentence'] = 'This, my friend, is a orthodox sentence.'
 info['target'] = info['sentence'].split(' ')[5]
 info['index'] = '5'
-
+info['lang'] = 'en'
 data = json.dumps(info)
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-s.connect(("localhost",1414))
+s.connect(("localhost",1616))
 
 print('Sending...')
 s.send(data+'\n')
--- TCP_simplifier_server/Galician_Test_Simplifier.py
+++ TCP_simplifier_server/Galician_Test_Simplifier.py
@@ -4,12 +4,12 @@ info = {}
 info['sentence'] = 'Os liquens que medran no residuo asfaltico suxiren a sua baixa toxicidade , que caberia comparar a dunha autoestrada .'
 info['target'] = info['sentence'].split(' ')[7]
 info['index'] = '7'
-
+info['lang'] = 'pt'
 data = json.dumps(info)
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-s.connect(("localhost",1515))
+s.connect(("localhost",1616))
 
 print('Sending...')
 s.send(data+'\n')

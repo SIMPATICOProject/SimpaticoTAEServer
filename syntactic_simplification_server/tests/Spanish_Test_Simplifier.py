ee3795164e6165cb764669a5a76b6696f50d4d83
carolscarton <carol.scarton@gmail.com>
2017-02-15 14:52:37 +0000

    Added syntactic simplifier

---
+++ syntactic_simplification_server/tests/Spanish_Test_Simplifier.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import socket, json
+import sys
+
+def loadResources(path):
+    #Open resource file:
+    f = open(path)
+    #Create resource map:
+    resources = {}
+    #Read resource paths:
+    for line in f:
+        data = line.strip().split('\t')
+        if data[0] in resources:
+            print 'Repeated resource name: ' + data[0] + '. Please change the name of this resource.'
+        resources[data[0]] = data[1]
+    f.close()
+    #Return resource database:
+    return resources
+
+configurations = loadResources('../../configurations.txt')
+
+doc = open('examples.es', "r")
+
+for s in doc.readlines():
+    info = {}
+    info['sentence'] = s.strip()
+    info['lang'] = 'es'
+
+    data = json.dumps(info)
+
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    s.connect(("localhost",int(configurations['ss_local_server_port'])))
+
+    print('Sending...')
+    s.send(data+'\n')
+    print info['sentence']
+    print('Receiving...')
+    resp = s.recv(1024).decode('utf8')
+    print resp
+    print
+    s.close()

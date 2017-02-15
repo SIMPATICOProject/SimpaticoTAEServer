0c6c1351eed8fb555f94fee871ce7392d7218cf7
carolscarton <carol.scarton@gmail.com>
2017-02-15 14:48:00 +0000

    Added sentences with syntanctic simplification examples

--- main_TAE_server/tests/Syntactic_English_Test.py
+++ main_TAE_server/tests/Syntactic_English_Test.py
@@ -1,6 +1,6 @@
 import urllib2
 
-url = 'http://localhost:8080/?type=syntactic&sentence=His%20fighting%20technique%20is%20very%20unorthodox%20.'
+url = 'http://localhost:8080/?type=syntactic&sentence=These%20organisations%20have%20been%20checked%20by%20us%20and%20should%20provide%20you%20with%20a%20quality%20service%20.'
 
 content = urllib2.urlopen(url).read()
 

673493ef712903c5d9188154796bea79d5a223bc
carolscarton <carol.scarton@gmail.com>
2017-05-14 12:57:22 +0100

    updating MUSST module

--- main_TAE_server/tests/Lexical_English_Test.py
+++
@@ -1,7 +0,0 @@
-import urllib2
-
-url = 'http://localhost:8080/?type=lexical&target=unorthodox&sentence=His%20fighting%20technique%20is%20very%20unorthodox%20.&index=5'
-
-content = urllib2.urlopen(url).read()
-
-print content
--- main_TAE_server/tests/Syntactic_Spanish_Test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import urllib2
-
-url = 'http://localhost:8080/?type=syntactic&sentence=Si%20la%20persona%20beneficiaria%20abandonase%20la%20estancia%20una%20vez%20iniciada%20,%20no%20tendrá%20derecho%20a%20ningún%20tipo%20de%20devolución%20.'
-
-content = urllib2.urlopen(url).read()
-
-print content

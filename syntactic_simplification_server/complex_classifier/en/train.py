6c7a48e6e81a282b4de89547110dc8bc2676311f
carolscarton <carol.scarton@gmail.com>
2017-08-02 13:00:20 +0100

    Solving complex classifier issues

--- syntactic_simplification_server/complex_classifier/en/train.py
+++ syntactic_simplification_server/complex_classifier/en/train.py
@@ -16,6 +16,7 @@ import string
 import pickle
 from sklearn.externals import joblib
 import string
+import kenlm
 
 sys.path[0:0] = ["../../simpatico_ss"]
 from simpatico_ss.util import Parser
@@ -57,7 +58,14 @@ class Train:
             recall += recall_score(labels[test], pred)
             r_dummy += recall_score(labels[test], pred_dummy)
 
-        return f1/10., acc/10., precision/10., recall/10., f1_dummy/10., acc_dummy/10., p_dummy/10., r_dummy/10.
+
+        final_p = precision/10.
+        final_r = recall/10.
+        final_p_dummy = p_dummy/10.
+        final_r_dummy = r_dummy/10.
+        f1 = 2*(final_p*final_r)/(final_p+final_r)
+        f1_dummy = 2*(final_p_dummy*final_r_dummy)/(final_p_dummy+final_r_dummy)
+        return f1, acc/10., final_p, final_r, f1_dummy, acc_dummy/10., final_p_dummy, final_r_dummy
 
 
 
--- syntactic_simplification_server/simpatico_ss/simpatico_ss/classify.py
+++ syntactic_simplification_server/simpatico_ss/simpatico_ss/classify.py
@@ -4,7 +4,6 @@ from sklearn.externals import joblib
 import sys
 import string
 
-
 class Classify:
 
 

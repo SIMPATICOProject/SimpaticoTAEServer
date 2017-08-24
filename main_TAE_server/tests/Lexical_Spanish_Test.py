9fad33249820cf6009058eed8b7d9aa0955edbc4
ghpaetzold <ghpaetzold@outlook.com>
2017-08-24 10:44:43 +0100

    Added Spanish lexical simplification test

---
+++ main_TAE_server/tests/Lexical_Spanish_Test.py
@@ -0,0 +1,7 @@
+import urllib2
+
+url = 'http://localhost:8080/?type=lexical&target=sentencia&sentence=Esta%20es%20una%20sentencia%20.&index=3'
+
+content = urllib2.urlopen(url).read()
+
+print content
